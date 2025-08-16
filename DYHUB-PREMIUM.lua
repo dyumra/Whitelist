55a47ce838c1fd39409a6cc1aaf8d89f0ca4612e
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-16 22:41:27 +0700

    Update DYHUB-PREMIUM.lua

--- DYHUB-PREMIUM.lua
+++ DYHUB-PREMIUM.lua
@@ -10,7 +10,7 @@ return {
     ["dyhub_01L01"] = {
         Tag = "DYHUB01",
         Time = nil,
-        Day = -1,
+        Day = 99999999,
         Key = "DYHUB-DYHUB01-ADMIN"
     },
     ["dyumradyumra"] = {
@@ -21,44 +21,44 @@ return {
     },
     ["Kyn8s"] = {
         Tag = "kyn8s",
-        Time = "8888888",
-        Day = "0",
+        Time = nil,
+        Day = 8888888,
         Key = "DYHUB-KYN8S-SUPPORT"
     },
     ["TH0PUM_KUNG"] = {
         Tag = "oszq_",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-OSZQ-ADMIN"
     },
     ["Fsaiohs"] = {
         Tag = "oszq_",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-FSAIOHS-Y200"
     },
     ["Go_Ertadx4"] = {
         Tag = "Free",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-FREE-UNLIMITED"
     },
     ["Hanehxo91"] = {
         Tag = "Free",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-FREE-UNLIMITED"
     },
     ["1234563210"] = {
         Tag = "oszq_",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-PUSSY-888"
     },
     ["182735417"] = {
         Tag = "oszq_",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-DICK-888"
     },
 
@@ -66,91 +66,103 @@ return {
     ["kagefym"] = {
         Tag = "itspect",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-ZXCVB-NMLKJ-HGFDS-APOIU-YTREW-LIFETIME"
     },
     ["Yavib_Aga"] = {
         Tag = "yavib",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-8JY5A-Q3RTW-IK7OL-FRJL2-IPL7W-LIFETIME"
     },
     ["ymh_is666"] = {
         Tag = "idkkkkk0813",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-QWERT-YUIOP-ASDFG-HJKLZ-XCVBN-LIFETIME"
     },
     ["MeowyBee"] = {
         Tag = "meowybee",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-4FJ8K-M1N2P-Q7RST-VWXYZ-U6L8F-LIFETIME"
     },
     ["itz_Lxx71"] = {
         Tag = "lxxx7.",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-ST6IW-0K7WG-L8TWG-2GR5L-MN5O1-LIFETIME"
     },
     ["stealbf9"] = {
         Tag = "nzmdlz_97886",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-3WTAB-KU6AG-P9WBA-OI5SA-YEGSY-LIFETIME"
     },
     ["DjScrew65"] = {
         Tag = "pizzaman7654",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-HU6SH-9KSN1-L8SHA-YTW5S-NA84A-LIFETIME"
     },
     ["CinderellaPT"] = {
         Tag = "_geisha",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-HYWU8-P9AJ3-M7LA9-0UWN3-4HS67-LIFETIME"
     },
     ["Masayoshi88"] = {
         Tag = "musashi_0940",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-Pauljames12300"
     },
     ["Monkeycheese3365"] = {
         Tag = "meboop90",
         Time = "Lifetime",
-        Day = "0",
+        Day = nil,
         Key = "DYHUB-JUYV2-A7OIQ-MJ42P-L8KQW-GD4A3-LIFETIME"
     },
     
     -- ====================== Buyer ==============================
     ["Denisamilcar7771"] = {
         Tag = "denisamilcar7778",
-        Time = "0",
-        Day = "7",
+        Time = nil,
+        Day = 7,
         Key = "DYHUB-XQJ29-M8K7H-PL4D6-2YWQ1-7BR85-KEY7DAY"
     },
 
     -- ====================== Booster ==============================
     ["jssjshszs762"] = {
         Tag = "rosenest_ag8w",
-        Time = "0",
-        Day = "8",
+        Time = nil,
+        Day = 8,
         Key = "DYHUB-ROSENEST-3DAY"
     },
 
     -- ====================== Giveaway =============================
     ["Hiimnew928289alt2"] = {
         Tag = "rip_v1p",
-        Time = "0",
-        Day = "12",
+        Time = nil,
+        Day = 12,
         Key = "DYHUB-KU7AH-Q2B5A-MKG5A-B5S4E-I7GA3-KEY14DAY"
     },
-    ["name"] = {
-        Tag = "name",
-        Time = "0",
-        Day = "0",
-        Key = "DYHUB-NOTHING"
+    ["MLG360Everest"] = {
+        Tag = "rockstarexpansion",
+        Time = nil,
+        Day = 1,
+        Key = "DYHUB-JUY5A-KEY1DAY"
+    },
+    ["uhhMeSksm"] = {
+        Tag = "sksmcutg",
+        Time = nil,
+        Day = 1,
+        Key = "DYHUB-HUTI5-KEY1DAY"
+    },
+    ["themostrealest_BACON"] = {
+        Tag = "notjapanese0",
+        Time = nil,
+        Day = 1,
+        Key = "DYHUB-JUIBA-KEY1DAY"
     },
 }
