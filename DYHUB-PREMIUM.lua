902335b158396421bdc40378572ab7acdbc852a1
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-16 19:33:33 +0700

    Update and rename DYHUB-PREMIUM to DYHUB-PREMIUM.lua

--- DYHUB-PREMIUM.lua
+++ DYHUB-PREMIUM.lua
@@ -1,130 +1,156 @@
--- PremiumUsers.lua
+-- DYHUB-PREMIUM.lua
 return {
     -- ====================== Admin ================================
     ["Yolmar_43"] = {
         Tag = "dyumraisgoodguy",
-        Time = "Days: 99999999",
+        Time = "0",
+        Day = "99999999",
         Key = "DYHUB-OWNER"
     },
     ["dyhub_01L01"] = {
         Tag = "DYHUB01",
-        Time = "Days: -1",
+        Time = "0",
+        Day = "-1",
         Key = "DYHUB-DYHUB01-ADMIN"
     },
     ["dyumradyumra"] = {
         Tag = "KUY",
-        Time = "Time: @Sigma",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-KUY-SIGMA"
     },
     ["Kyn8s"] = {
         Tag = "kyn8s",
-        Time = "Days: 7",
+        Time = "8888888",
+        Day = "0",
         Key = "DYHUB-KYN8S-SUPPORT"
     },
     ["TH0PUM_KUNG"] = {
         Tag = "oszq_",
-        Time = "Days: -1",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-OSZQ-ADMIN"
     },
     ["Fsaiohs"] = {
         Tag = "oszq_",
-        Time = "Year: 200",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-FSAIOHS-Y200"
     },
     ["Go_Ertadx4"] = {
         Tag = "Free",
-        Time = "Days: -1",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-FREE-UNLIMITED"
     },
     ["Hanehxo91"] = {
         Tag = "Free",
-        Time = "Days: -1",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-FREE-UNLIMITED"
     },
     ["1234563210"] = {
         Tag = "oszq_",
-        Time = "Days: -1",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-PUSSY-888"
     },
     ["182735417"] = {
         Tag = "oszq_",
-        Time = "Days: -1",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-DICK-888"
     },
 
     -- ====================== Buyer ================================
     ["kagefym"] = {
         Tag = "itspect",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-ZXCVB-NMLKJ-HGFDS-APOIU-YTREW-LIFETIME"
     },
     ["Yavib_Aga"] = {
         Tag = "yavib",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-8JY5A-Q3RTW-IK7OL-FRJL2-IPL7W-LIFETIME"
     },
     ["ymh_is666"] = {
         Tag = "idkkkkk0813",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-QWERT-YUIOP-ASDFG-HJKLZ-XCVBN-LIFETIME"
     },
     ["MeowyBee"] = {
         Tag = "meowybee",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-4FJ8K-M1N2P-Q7RST-VWXYZ-U6L8F-LIFETIME"
     },
     ["itz_Lxx71"] = {
         Tag = "lxxx7.",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-ST6IW-0K7WG-L8TWG-2GR5L-MN5O1-LIFETIME"
     },
     ["stealbf9"] = {
         Tag = "nzmdlz_97886",
-        Time = "Times: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-3WTAB-KU6AG-P9WBA-OI5SA-YEGSY-LIFETIME"
     },
     ["DjScrew65"] = {
         Tag = "pizzaman7654",
-        Time = "Time: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-HU6SH-9KSN1-L8SHA-YTW5S-NA84A-LIFETIME"
     },
     ["CinderellaPT"] = {
         Tag = "_geisha",
-        Time = "Time: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-HYWU8-P9AJ3-M7LA9-0UWN3-4HS67-LIFETIME"
     },
     ["Masayoshi88"] = {
         Tag = "musashi_0940",
-        Time = "Time: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-Pauljames12300"
-    },
-    ["Denisamilcar7771"] = {
-        Tag = "denisamilcar7778",
-        Time = "Days: 7",
-        Key = "DYHUB-XQJ29-M8K7H-PL4D6-2YWQ1-7BR85-KEY7DAY"
     },
     ["Monkeycheese3365"] = {
         Tag = "meboop90",
-        Time = "Time: Lifetime",
+        Time = "Lifetime",
+        Day = "0",
         Key = "DYHUB-JUYV2-A7OIQ-MJ42P-L8KQW-GD4A3-LIFETIME"
+    },
+    
+    -- ====================== Buyer ==============================
+    ["Denisamilcar7771"] = {
+        Tag = "denisamilcar7778",
+        Time = "0",
+        Day = "7",
+        Key = "DYHUB-XQJ29-M8K7H-PL4D6-2YWQ1-7BR85-KEY7DAY"
     },
 
     -- ====================== Booster ==============================
     ["jssjshszs762"] = {
         Tag = "rosenest_ag8w",
-        Time = "Days: 8",
+        Time = "0",
+        Day = "8",
         Key = "DYHUB-ROSENEST-3DAY"
     },
 
     -- ====================== Giveaway =============================
     ["Hiimnew928289alt2"] = {
         Tag = "rip_v1p",
-        Time = "Days: 12",
+        Time = "0",
+        Day = "12",
         Key = "DYHUB-KU7AH-Q2B5A-MKG5A-B5S4E-I7GA3-KEY14DAY"
     },
-    ["0"] = {
-        Tag = "0",
-        Time = "Days: 3",
-        Key = "DYHUB-GIVEAWAY-3DAY"
+    ["name"] = {
+        Tag = "name",
+        Time = "0",
+        Day = "0",
+        Key = "DYHUB-NOTHING"
     },
 }
