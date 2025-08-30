ee07bdf745b03044e64c1e2480cdebea88e9f04d
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-08-30 20:21:50 +0700

    Update DYHUB-PREMIUM.lua

--- DYHUB-PREMIUM.lua
+++ DYHUB-PREMIUM.lua
@@ -171,7 +171,7 @@ return {
         Day = 7,
         Key = "DYHUB-HY6AG-BH68A-L9P87-E3R2Q-K8U64-KEY7DAY"
     },
-    ["Bestdh81"] = {
+    ["COOLGAMERSECOND51"] = {
         Tag = "aw03093",
         Time = nil,
         Day = 3,
